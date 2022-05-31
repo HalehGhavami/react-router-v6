09882e7b52ba2f94e550434415410d7fe5e59c9c
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-31 09:40:13 +0200

    directed to specified page using url path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import About from './components/about';
-// import Home from './components/home';
-
-ReactDOM.render(<About />, document.getElementById('root'));
+import Home from './components/home';
+//using "as " for naming shorter
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//BrowerRouter(Router) have to wrapp all of our app
+//at v6 all Route have to be in Routes component
+//Add Route as single at 3rd level
+ReactDOM.render(
+  <Router>
+    <Routes>
+      {/* element attribute instaed of component attribute in last version */}
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      {/* rendering Home with both excat / and /home in path  without need to use "excat" or Switch*/}
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </Router>,
+  document.getElementById('root')
+);
