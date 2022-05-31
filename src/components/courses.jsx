2f24183c8c44ac95f146d3f689df833aa52797b4
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2022-05-31 11:33:56 +0200

    used <NavLink> to show inActive  page link

--- src/components/courses.jsx
+++ src/components/courses.jsx
@@ -1,12 +1,28 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 //for nesting Route
 import { Outlet } from 'react-router-dom';
 const Courses = () => {
   return (
     <>
       <h1>Courses Page</h1>
-      <Link to={'/courses/course1'}>Course1</Link> |
-      <Link to={'/courses/course2'}>Course2</Link>
+      {/* distructe isActive item of object (that is the only item of this object) */}
+      <NavLink
+        style={({ isActive }) => {
+          return { color: isActive ? 'red' : 'black' };
+        }}
+        to={'/courses/course1'}
+      >
+        Course1
+      </NavLink>
+      |
+      <NavLink
+        style={({ isActive }) => {
+          return { color: isActive ? 'red' : 'black' };
+        }}
+        to={'/courses/course2'}
+      >
+        Course2
+      </NavLink>
       <Outlet />
     </>
   );
